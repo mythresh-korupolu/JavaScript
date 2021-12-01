b4f1852cd93a606ed648ab937340a89bd637aeca
mythresh-korupolu <58387658+mythresh-korupolu@users.noreply.github.com>
2021-12-01 07:12:22 +0530

    commit

---
+++ OperatorsandDatatypes.js
@@ -0,0 +1,12 @@
+//Data Types and Operators
+let a=10
+let b=20
+let c=3.400 
+let s="Operators" // String 
+console.log(a+b) //addition
+console.log(a-b) // subtraction
+console.log(a*b) //multiplication
+console.log(a**b) //powers
+console.log(a/2) // Quotient
+console.log(c%2) // Remainder
+console.log(s); // String
