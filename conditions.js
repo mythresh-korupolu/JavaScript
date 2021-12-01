07bda9bb2cf406c490d219bf29bca139a8525036
mythresh-korupolu <58387658+mythresh-korupolu@users.noreply.github.com>
2021-12-01 07:25:59 +0530

    commit

---
+++ conditions.js
@@ -0,0 +1,38 @@
+// Conditions in JavaScript
+// if Statement
+let  b=30
+if(b==30)
+{
+    console.log("It is a Same number")
+}
+
+// if else condition
+let age=30
+if (age >18)
+{
+    console.log("Eligible to Vote")
+}
+else
+{
+    console.log("Not Eligible to Vote")  
+}
+// Switch Conditions
+let day=7;
+switch(day){
+    case 1: console.log("Sunday")
+        break;
+    case 2:console.log("Monday")
+        break;
+    case 3:console.log("Tuesday")
+        break;
+    case 4: console.log("wednesday")
+        break;
+    case 5:console.log("Thrusday")
+        break
+    case 6: console.log("Friday")
+        break
+    case 7: console.log("Saturday")
+        break
+}
+ 
+
